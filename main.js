4059f18e79d1d7b04e625996a3450ed8f9eae3b2
tonylampada <tonylampada@gmail.com>
2018-05-19 09:01:27 -0300

    adicionar e remover do preview

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var app = new Vue({
   el: '#app',
   data: {
-    items: [],
-    selected: [],
+    all_items: [],
+    // selected: [],
     selected_items: [],
     selectAll: false,
   },
@@ -10,32 +10,42 @@ var app = new Vue({
     list_items(){
       axios.get('https://api.github.com/repos/rg3915/django-experience/issues')
       .then((result) => {
-        this.items = result.data.map((item) => {
+        this.all_items = result.data.map((item) => {
           return {number: item.number, id: item.id, title: item.title, state: item.state, url: item.url}
         })
       })
     },
-    select(){
-      this.selected = [];
-      this.selected_items = [];
-      if (!this.selectAll) {
-        for (let i in this.items) {
-          this.selected.push(this.items[i].number);
-        }
-        for (let j in this.items) {
-          this.selected_items.push(
-            {number: this.items[j].number, id: this.items[j].id, title: this.items[j].title, state: this.items[j].state, url: this.items[j].url}
-          );
-        }
-      }
+    addToPreview(){
+      this.selected_items = this.all_items.filter(item => item.selected)
+      this.selected_items.map(item => {
+        item.selected = false
+      })
+      this.all_items = this.all_items.filter(item => !item.selected)
+    },
+    removeFromPreview(){
+      window.alert('Exercicio pro Regis')
     }
+    // select(){
+    //   this.selected = [];
+    //   this.selected_items = [];
+    //   if (!this.selectAll) {
+    //     for (let i in this.items) {
+    //       this.selected.push(this.items[i].number);
+    //     }
+    //     for (let j in this.items) {
+    //       this.selected_items.push(
+    //         {number: this.items[j].number, id: this.items[j].id, title: this.items[j].title, state: this.items[j].state, url: this.items[j].url}
+    //       );
+    //     }
+    //   }
+    // }
   },
   watch: {
-    selected(value){
-      this.selected_items = this.items.filter(
-        i => value.includes(i.number)
-      );
-    }
+    // selected(value){
+    //   this.selected_items = this.items.filter(
+    //     i => value.includes(i.number)
+    //   );
+    // }
   },
   mounted(){
     this.list_items()
